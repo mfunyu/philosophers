ac135388f3ccc4d61054a20a6637f05ba8e1b4b5
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-26 23:08:50 +0900

    [add] exec_func_in_mutex.c

---
+++ srcs/exec_func_in_mutex.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_func_in_mutex.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 23:00:21 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/08/26 23:07:39 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	exec_func_in_mutex(t_info *info, int arg, int func(t_info *, int))
+{
+	int		ret;
+
+	ret = 0;
+	pthread_mutex_lock(&(info->shared->mutex));
+	(*func)(info, arg);
+	pthread_mutex_unlock(&(info->shared->mutex));
+	return (ret);
+}
