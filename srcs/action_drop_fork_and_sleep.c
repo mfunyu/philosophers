aba589c40ea28f79ac271be6ed86df46807e5324
mfunyu <mfunyu@student.42tokyo.jp>
2021-09-15 16:31:02 +0200

    [fix] mutex dropfork timeing

--- srcs/action_drop_fork_and_sleep.c
+++ srcs/action_drop_fork_and_sleep.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 16:35:23 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/09/14 17:15:19 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/09/15 16:18:14 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	action_drop_fork_and_sleep(t_info *info)
 	if (info->philo_id == info->shared->nb_philos)
 		right = 0;
 	left = info->philo_id - 1;
+	print_timestamp_log(info, SLEEP);
 	pthread_mutex_lock(&info->shared->mutex_forks[right]);
 	info->shared->arr_forks[right] = 0;
+	pthread_mutex_unlock(&info->shared->mutex_forks[right]);
 	pthread_mutex_lock(&info->shared->mutex_forks[left]);
 	info->shared->arr_forks[left] = 0;
-	print_timestamp_log(info, SLEEP);
-	pthread_mutex_unlock(&info->shared->mutex_forks[right]);
 	pthread_mutex_unlock(&info->shared->mutex_forks[left]);
 	ms_sleep(info->shared->time2sleep);
 	return (SUCCESS);
