6ffb80c93c891515a85cb121cac391e56d84c5fe
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-30 23:40:41 +0900

    [update] add color for printing logs

--- srcs/time/print_timestamp_log.c
+++ srcs/time/print_timestamp_log.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 16:00:43 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/25 17:54:46 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/25 18:33:11 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,30 @@
 #include "threads.h"
 #include "enums.h"
 #include "exit.h"
+#include "color.h"
 #include <stdio.h>
 #include <pthread.h>
+
+static char	*_get_color(t_action action)
+{
+	if (action == DIE)
+		return (C_DIE);
+	else if (action == FORK)
+		return (C_FORK);
+	else if (action == EAT)
+		return (C_EAT);
+	else if (action == SLEEP)
+		return (C_SLEEP);
+	else if (action == THINK)
+		return (C_THINK);
+	return (NULL);
+}
 
 int64_t	print_timestamp_log(t_info *info, t_action action)
 {
 	char	*status;
 	int64_t	timestamp_in_ms;
 
-	status = "";
 	if (is_eos(info))
 		return (ERROR);
 	else if (action == DIE)
@@ -39,7 +54,8 @@ int64_t	print_timestamp_log(t_info *info, t_action action)
 	timestamp_in_ms = get_time_in_ms();
 	if (timestamp_in_ms == ERROR)
 		return (ERROR);
-	printf("%"PRId64" %d %s\n", timestamp_in_ms, info->philo_id, status);
+	printf("%s%"PRId64" %d %s%s\n",
+		_get_color(action), timestamp_in_ms, info->philo_id, status, C_RESET);
 	if (action == DIE)
 		return (ERROR);
 	pthread_mutex_unlock(&info->shared->mutexes[PRINT]);
